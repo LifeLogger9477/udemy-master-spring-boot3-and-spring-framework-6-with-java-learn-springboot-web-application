710dae06e331398596aa321fb8b52c24051b5f7b
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-11 12:50:36 +0900

    섹션 07:Step 04.01 - Enhancing Spring MVC Controller to provide HTML response

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -20,4 +20,21 @@ public class SayHelloController {
 
     return "Hello! What are you learning today?";
   }
+
+  @ResponseBody
+  @RequestMapping (value = "/say-hello-html")
+  public String sayHelloHtmml() {
+
+    StringBuffer sb = new StringBuffer();
+    sb.append("<html>");
+    sb.append("<head>");
+    sb.append("<title>My first HTML Page</title>");
+    sb.append("</head>");
+    sb.append("<body>");
+    sb.append("My first html page with body");
+    sb.append("</body>");
+    sb.append("</html>");
+
+    return sb.toString();
+  }
 }
