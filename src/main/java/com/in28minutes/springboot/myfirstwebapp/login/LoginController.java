3382b0ef375aa055809dba59ca104d2463435b27
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-15 12:21:29 +0900

    섹션 07:Step 11 - Creating a Login Form : form method=get or post.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,9 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * author : ms.Lee
@@ -12,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+  // login
   @RequestMapping (value = "/login")
-  public String gotoLoginPage(@RequestParam String name, ModelMap modelMap) {
-
-    modelMap.put( "name", name );
+  public String gotoLoginPage() {
 
     return "login";
   }
