ff4b68165675fd3b2fbefddd95ea8deaab1c322f
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-15 12:02:13 +0900

    섹션 07:Step 09 - Quick Overview Importantce of Logging with Spring Boot : LoginController.java 에 테스트 코드 추가 부분 및 주석 삭제.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,7 +1,5 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-  private Logger logger = LoggerFactory.getLogger( getClass() );
-
-  // /login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
-
-  // http://localhost:8080/login?name=Ranga
-
-  // Model 개념 도입
   @RequestMapping (value = "/login")
   public String gotoLoginPage(@RequestParam String name, ModelMap modelMap) {
 
     modelMap.put( "name", name );
-
-    logger.debug( "Request param is {}", name );
-    logger.info( "I want this printed ata info level." );
-    logger.warn( "I want this printed ata warn level." );
-    // NOT RECOMMENDED FOR PROD CODE
-    System.out.println("Request param is " + name);
 
     return "login";
   }
