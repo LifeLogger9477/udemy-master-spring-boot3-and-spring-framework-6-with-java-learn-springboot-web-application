6597236bf3f3ab6433319830d1f10d696f9a3f23
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-25 14:14:27 +0900

    섹션 07:Step 35.01 - Configuring Spring Security to Get H2 console Working : Security 설정을 변경하여 접속할 수 있도록 진행. 6.2에 맞춰서 작성함

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,17 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * author : ms.Lee
@@ -56,5 +62,30 @@ public class SpringSecurityConfiguration {
   public PasswordEncoder passwordEncoder() {
 
     return new BCryptPasswordEncoder();
+  }
+
+  // all urls are protected
+  // a login form is shown for unauthorized requests
+  // csrf disable
+  // frames
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http.authorizeHttpRequests(
+        auth -> auth.anyRequest().authenticated()
+    );
+
+    http.formLogin( withDefaults());
+
+    http.csrf( AbstractHttpConfigurer::disable );
+    http.headers(
+        headersConfigurer ->
+            headersConfigurer.frameOptions(
+                HeadersConfigurer.FrameOptionsConfig::disable
+            )
+    );
+
+    return http.build();
   }
 }
